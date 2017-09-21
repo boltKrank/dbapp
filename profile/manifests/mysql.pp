97dad923786b0cfcd63b4af69632927cc3f4cb25
root <root@webapp.localdomain>
2017-09-21 05:58:39 -0700

    Mysql profile notes

--- profile/manifests/mysql.pp
+++ profile/manifests/mysql.pp
@@ -1,5 +1,19 @@
 class profile::mysql{
-	
+
+#Firewall on ?
+
+#binding 127.0.0.1 -> 0.0.0.0
+
+#CREATE USER 'puppet'@'localhost' IDENTIFIED BY 'puppetlabs';
+#GRANT ALL PRIVILEGES ON *.* TO 'puppet'@'localhost' WITH GRANT OPTION;
+#CREATE USER 'puppet'@'%' IDENTIFIED BY 'puppetlabs';
+#GRANT ALL PRIVILEGES ON *.* TO 'puppet'@'%' WITH GRANT OPTION;
+#FLUSH PRIVILEGES;
+
+
+	class { '::mysql::server':
+		root_password => 'puppetlabs',
+	}	
 
 
 }
